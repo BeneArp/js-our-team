4d9696f75e677eca5472a0c9df02844a53e8a7cf
BeneArp <benedetta.arpino@gmail.com>
2024-06-18 15:27:55 +0200

    feat: add array of objects Milestone 0 completed

--- js/script.js
+++ js/script.js
@@ -19,4 +19,43 @@
 
 
 
-console.log("ciao");
+// creo un array di oggetti con le informazioni per ogni membro del team
+const membriTeam = [
+    {
+        "nome": "Wayne Barnett",
+        "ruolo": "Founder & CEO",
+        "immagine": "img/wayne-barnett-founder-ceo.jpg",
+    },
+
+    {
+        "nome": "Angela Caroll",
+        "ruolo": "Chief Editor",
+        "immagine": "img/angela-caroll-chief-editor.jpg",
+    },
+
+    {
+        "nome": "Walter Gordon",
+        "ruolo": "Office Manager",
+        "immagine": "img/walter-gordon-office-manager.jpg",
+    },
+
+    {
+        "nome": "Angela Lopez",
+        "ruolo": "Social Media Manager",
+        "immagine": "img/angela-lopez-social-media-manager.jpg",
+    },
+
+    {
+        "nome": "Soctt Estrada",
+        "ruolo": "Developer",
+        "immagine": "img/scott-estrada-developer.jpg",
+    },
+
+    {
+        "nome": "Barbara Ramos",
+        "ruolo": "Graphic Designer",
+        "immagine": "img/barbara-ramos-graphic-designer.jpg",
+    },
+];
+
+console.log(membriTeam);
